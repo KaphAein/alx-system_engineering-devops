50efb1e5faadbd0107f45d6d8952a276f8f9f9d6
kaphaein <cherifafadaly@gmail.com>
2024-05-20 15:47:54 +0000

    devops

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,12 +14,17 @@ if __name__ == "__main__":
         sys.exit(1)
 
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    if not username:
+        sys.exit(1)
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    csv_filename = "{}.csv".format(user_id)
+    try:
+        with open(csv_filename, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for t in todos:
+                writer.writerow([user_id, username, t.get("completed"), t.get("title")])
+    except IOError as e:
+        sys.exit(1)
