31120431b3f51a47a6491a009811e77befa90cfc
kaphaein <cherifafadaly@gmail.com>
2024-05-20 16:57:53 +0000

    devops

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     user = requests.get(url + "users").json()
 
     try:
-         with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        with open("todo_all_employees.json", "w") as jsonfile:
+            json.dump({
+                u.get("id"): [{
+                    "task": t.get("title"),
+                    "completed": t.get("completed"),
+                    "username": u.get("username")
+                } for t in requests.get(url + "todos",
+                                        params={"userId": u.get("id")}).json()]
+                for u in users}, jsonfile)
     except IOError as e:
         sys.exit(1)
