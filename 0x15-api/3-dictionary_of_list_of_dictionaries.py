5fc5e83cf0f0cc9c14d271df468d3a8f5e882deb
kaphaein <cherifafadaly@gmail.com>
2024-05-20 16:56:44 +0000

    devops

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users").json()
 
-    json_filename = "{}.json".format(user_id)
     try:
-        with open("todo_all_employees.json", "w") as jsonfile:
-            json.dump({u.get("id"): [{
+         with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": u.get("username")
