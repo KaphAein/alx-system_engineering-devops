1ff9c8e7eec5b5aa2e3d4421b5b5979e461fde69
kaphaein <cherifafadaly@gmail.com>
2024-05-20 16:52:09 +0000

    devops

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
+import json
+import requests
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+
+    json_filename = "{}.json".format(user_id)
+    try:
+        with open("todo_all_employees.json", "w") as jsonfile:
+            json.dump({u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
+    except IOError as e:
+        sys.exit(1)
