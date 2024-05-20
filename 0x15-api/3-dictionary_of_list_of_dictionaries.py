3151dd35dc8a500104236f56d78ef988a99e4a27
kaphaein <cherifafadaly@gmail.com>
2024-05-20 17:22:44 +0000

    devops

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
 if __name__ == "__main__":
-
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users").json()
+    users = requests.get(url + "users").json()
 
-    try:
-        with open("todo_all_employees.json", "w") as jsonfile:
-            json.dump({
-                u.get("id"): [{
-                    "task": t.get("title"),
-                    "completed": t.get("completed"),
-                    "username": u.get("username")
-                } for t in requests.get(url + "todos",
-                                        params={"userId": u.get("id")}).json()]
-                for u in users}, jsonfile)
-    except IOError as e:
-        sys.exit(1)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
