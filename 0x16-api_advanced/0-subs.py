235e5bb130eececc674aac19af7c07c98110d287
kaphaein <cherifafadaly@gmail.com>
2024-06-04 16:54:05 +0000

    \API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
