9b97c18f5fee380ee8370ec30378edc4f78d803c
kaphaein <cherifafadaly@gmail.com>
2024-06-04 16:48:58 +0000

    \API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,17 @@ import requests
 def number_of_subscribers(subreddit):
     '''queries the Reddit API and returns the number of subscribers'''
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'subreddit-subscriber-counter/0.1'}
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    headers = {
+         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
         
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
+
 
